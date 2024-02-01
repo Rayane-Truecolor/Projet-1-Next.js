77c6d5a5389c79308796fecff619dd09e236d5b5
Rayane-Truecolor <hassaine.ray@gmail.com>
2024-02-01 23:02:39 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Seo } from "@/ui/components/seo";
+
+export default function Home() {
+  return (
+    <>
+    <Seo title ="Coders Monkeys" description="Description..."/>
+   <h1>hello world</h1>
+     
+      </>
+  );
+}
