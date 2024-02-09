9fce8f521590334757ea7fd50e2d7f019485e25a
Rayane-Truecolor <hassaine.ray@gmail.com>
2024-02-09 14:07:20 +0100

    Ajout du spinner

---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCx1HwyyB3l_zINJQan8YLE7MnPeRQu2ng",
+  authDomain: "codersmonkey-2-c3e40.firebaseapp.com",
+  projectId: "codersmonkey-2-c3e40",
+  storageBucket: "codersmonkey-2-c3e40.appspot.com",
+  messagingSenderId: "674016310480",
+  appId: "1:674016310480:web:e382c1a12eb1d1cf6a1ad5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,75 @@
 import { Seo } from "@/ui/components/seo";
+import { Button } from "@/ui/design-system/button/button";
+import { Spinner } from "@/ui/design-system/spinner/spinner";
+import { Typography } from "@/ui/design-system/typography/typography";
+import { RiAncientGateFill, RiUser6Fill } from "react-icons/ri";
 
 export default function Home() {
   return (
-    <>
-    <Seo title ="Coders Monkeys" description="Description..."/>
-   <h1>hello world</h1>
-     
-      </>
+    <>      
+    <Seo title="Coders Monkeys" description="Description..." />
+
+
+    <Spinner />
+
+<div className="flex items-center gap-4 p-10">
+<Button size="small" icon={({ icon: RiUser6Fill })} >Accent</Button>
+
+<Button size="small" icon={({ icon: RiUser6Fill })} iconPosition="left" >Accent</Button>
+
+<Button size="small" variant="secondary">  Secondary  </Button>
+<Button size="small" variant="outline">  Accent  </Button>
+<Button size="small" variant="disabled" disabled>  Accent  </Button>
+<Button size="small" variant="ico" icon={({ icon: RiUser6Fill })}/>   
+
+
+
+</div>
+
+<div className="flex items-center gap-4 p-10">
+<Button>Accent</Button>
+<Button variant="secondary">  Secondary  </Button>
+<Button variant="outline">  Accent  </Button>
+<Button variant="disabled" disabled>  Accent  </Button>
+<Button variant="ico" icon={({ icon: RiUser6Fill })}/>   
+
+
+</div>
+
+<div className="flex items-center gap-4 p-10">
+<Button size="large">Accent</Button>
+<Button size="large" variant="secondary">  Secondary  </Button>
+<Button size="large" variant="outline">  Accent  </Button>
+<Button size="large" variant="disabled" disabled>  Accent  </Button>
+<Button size="large" variant="ico" icon={({ icon: RiUser6Fill })} />   
+<Button size="large" variant="ico" icon={({ icon: RiUser6Fill })} iconTheme="gray"/>   
+<Button size="large" variant="ico" icon={({ icon: RiUser6Fill })} iconTheme="secondary"/>   
+
+
+</div>
+  
+
+
+
+    
+   {/* <div className="space-y-5">
+      <Typography>Coders Monkeys</Typography>
+      <Typography theme="primary" variant="h1" component="div">
+        Coders Monkeys
+      </Typography>
+      <Typography theme="secondary" variant="lead" component="div">
+        Coders Monkeys
+      </Typography>
+      <Typography variant="body-sm" component="div">
+        Coders Monkeys
+      </Typography>
+      <Typography variant="caption4" component="div">
+        Coders Monkeys
+      </Typography>
+      <Typography variant="caption4" weight="medium" component="div">
+        Coders Monkeys
+      </Typography>
+  </div>*/}
+    </>
   );
 }
